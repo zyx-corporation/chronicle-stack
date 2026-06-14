78fad8800984b06224bbcfc03805ac4e6d8025b8
Tomoyuki Kano <tomyuk@zyxcorp.jp>
2026-06-14 17:33:38 +0900

    feat: export classification metadata models

--- src/chronicle/models/__init__.py
+++ src/chronicle/models/__init__.py
@@ -1,4 +1,5 @@
 from chronicle.models.artifact import Artifact, ArtifactVersion
+from chronicle.models.classification import ClassificationLayer, ClassificationMetadata, LlmPolicy, Sensitivity
 from chronicle.models.context import Context, ContextScope
 from chronicle.models.decision import Decision
 from chronicle.models.event import ChronicleEvent
@@ -12,10 +13,14 @@ __all__ = [
     "ArtifactVersion",
     "ChronicleEvent",
     "ChronicleMetadata",
+    "ClassificationLayer",
+    "ClassificationMetadata",
     "Context",
     "ContextScope",
     "Decision",
+    "LlmPolicy",
     "RdeDiffRecord",
+    "Sensitivity",
     "SourceProvenance",
     "VisibilityHint",
 ]
